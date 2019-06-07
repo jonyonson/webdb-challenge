be9e7861c9496a533d8e27c3632e102e09bd67d6
Jonathan Taylor <jonyonson@icloud.com>
2019-06-07 12:56:07 -0500

    STRETCH: added the remaining CRUD operations for projects

--- data/helpers/projects-model.js
+++ data/helpers/projects-model.js
@@ -4,6 +4,8 @@ module.exports = {
   addProject,
   getProjects,
   getProjectById,
+  removeProject,
+  updateProject,
 };
 
 function addProject(project) {
@@ -20,4 +22,21 @@ async function getProjectById(id) {
 
   project[0].actions = actions;
   return project[0];
+}
+
+async function removeProject(id) {
+  const project = await projectsDb('projects')
+    .where({ id })
+    .del();
+  await projectsDb('actions')
+    .where({ project_id: id })
+    .del();
+
+  return project;
+}
+
+function updateProject(id, changes) {
+  return projectsDb('projects')
+    .where({ id })
+    .update(changes);
 }
--- routes/projects-router.js
+++ routes/projects-router.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
+
 const {
   addProject,
   getProjects,
   getProjectById,
+  removeProject,
+  updateProject,
 } = require('../data/helpers/projects-model');
 
 router.get('/', (req, res) => {
@@ -32,6 +35,36 @@ router.get('/:id', (req, res) => {
     })
     .catch(error => {
       res.status(500).json(error);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  removeProject(req.params.id)
+    .then(deleted => {
+      if (deleted > 0) {
+        res.status(200).end();
+      } else {
+        res.status(404).json({ message: 'Project not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: error });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  updateProject(id, changes)
+    .then(updated => {
+      if (updated > 0) {
+        res.status(200).json(updated);
+      } else {
+        res.status(404).json({ message: 'Project not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: error });
     });
 });
 
