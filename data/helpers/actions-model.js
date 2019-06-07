ebe8e83940a2383f4843555fb5d47a60e64f7efd
Jonathan Taylor <jonyonson@icloud.com>
2019-06-07 12:19:30 -0500

    STRETCH: added the remaining CRUD operations for actions

--- data/helpers/actions-model.js
+++ data/helpers/actions-model.js
@@ -1,6 +1,12 @@
 const actionsDb = require('../dbConfig');
 
-module.exports = { addAction, getActions };
+module.exports = {
+  addAction,
+  getActions,
+  getActionById,
+  removeAction,
+  updateAction,
+};
 
 function addAction(action) {
   return actionsDb('actions').insert(action);
@@ -8,4 +14,20 @@ function addAction(action) {
 
 function getActions() {
   return actionsDb('actions');
+}
+
+function getActionById(id) {
+  return actionsDb('actions').where({ id });
+}
+
+function removeAction(id) {
+  return actionsDb('actions')
+    .where({ id })
+    .del();
+}
+
+function updateAction(id, changes) {
+  return actionsDb('actions')
+    .where({ id })
+    .update(changes);
 }
--- routes/actions-router.js
+++ routes/actions-router.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
-const { addAction, getActions } = require('../data/helpers/actions-model');
+
+const {
+  addAction,
+  getActions,
+  getActionById,
+  removeAction,
+  updateAction,
+} = require('../data/helpers/actions-model');
 
 router.get('/', (req, res) => {
   getActions()
@@ -15,6 +22,50 @@ router.post('/', (req, res) => {
   addAction(req.body)
     .then(action => {
       res.status(201).json(action);
+    })
+    .catch(error => {
+      res.status(500).json({ message: error });
+    });
+});
+
+router.get('/:id', (req, res) => {
+  getActionById(req.params.id)
+    .then(action => {
+      if (action.length > 0) {
+        res.status(200).json(action);
+      } else {
+        res.status(404).json({ message: 'Action not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: error });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  removeAction(req.params.id)
+    .then(deleted => {
+      if (deleted > 0) {
+        res.status(200).end();
+      } else {
+        res.status(404).json({ message: 'Action not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: error });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  updateAction(id, changes)
+    .then(updated => {
+      if (updated > 0) {
+        res.status(200).json(updated);
+      } else {
+        res.status(404).json({ message: 'Action not found' });
+      }
     })
     .catch(error => {
       res.status(500).json({ message: error });
